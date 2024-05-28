ea5ddd84d38cdf607cf644847a2cd559605b56fa
TzurelRauper <tzurel150@gmail.com>
2024-05-28 17:57:41 +0300

    add functions to AdminPage.jsx

--- Frontend/RestaurantProject/src/AdminPage.jsx
+++ Frontend/RestaurantProject/src/AdminPage.jsx
@@ -6,28 +6,48 @@ import Input from '@mui/joy/Input';
 import { useState } from 'react';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
 import Button from '@mui/material/Button';
+import './App.css'
 
 const AdminPage = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isActive, setIsActive] = useState('hidden');
+    const regularExpCheckValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+    const handleChange = (event) => {
+        event.target.placeholder === 'Email*' ? setEmail(event.target.value) : '';
+        event.target.placeholder === 'Password*' ? setPassword(event.target.value) : '';        
+    }
+
+    const signInClick = () => {
+        if(regularExpCheckValidEmail.test(email) && password !== '') {
+            setIsActive('hidden');
+            console.log('need to work :)');
+        } else {
+            setIsActive('');
+        }
+    }
+    
     return (
     <div>
         <h1 style={{color: '#9FF6FF'}}>Login  <LockOpenIcon/></h1>
         <div>
         <FormControl>
         <FormLabel style={{color: '#1CCBFF', fontSize: '20px'}}>Email Adress:</FormLabel>
-        <Input placeholder="Email*" />
+        <Input placeholder="Email*" onChange={handleChange}/>
         </FormControl>
         </div>
         <div style={{marginTop: '20px'}}>
         <FormControl>
         <FormLabel style={{color: '#1CCBFF', fontSize: '20px'}}>Password:</FormLabel>
-        <Input placeholder="Password*" />
+        <Input placeholder="Password*" onChange={handleChange}/>
         </FormControl>
         </div>
-        <Button style={{marginTop: '20px'}} variant="contained">SIGN IN</Button>
+        <Button style={{marginTop: '20px'}} variant="contained" onClick={signInClick}>SIGN IN</Button>
+        <div>
+            <p className={isActive} style={{color: '#FF2121'}}>Your Email or Password invalid</p>
+        </div>
         <div style={{marginTop: '20px', marginRight: '120px'}}>
             <Button>Forgot password?</Button>
         </div>
