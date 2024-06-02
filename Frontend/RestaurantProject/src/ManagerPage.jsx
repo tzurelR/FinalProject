2a6e3e3297c9320d7716ad8de4514ab2a0e67517
TzurelRauper <tzurel150@gmail.com>
2024-06-02 19:05:02 +0300

    add icons to buttons in ManagerPage.jsx

--- Frontend/RestaurantProject/src/ManagerPage.jsx
+++ Frontend/RestaurantProject/src/ManagerPage.jsx
@@ -1,21 +1,28 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import TableRestaurantIcon from '@mui/icons-material/TableRestaurant';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
+import SetMealIcon from '@mui/icons-material/SetMeal';
 
 const ManagerPage = () => {
+    
+    
+
     return (
         <div className='managerPageDiv'>
             <div className='buttonsManagerPage'>
             <Button style={{fontSize: '1.1em'}} className='side-button' variant="contained" color="secondary">
-            Get Orders
+            Get Orders <FastfoodIcon/>
             </Button>
             <Button style={{marginTop: '40px', fontSize: '1.1em'}} className='side-button' variant="contained" color="secondary">
-            Get Table Reservations
+            Get Table Reservations <TableRestaurantIcon/>
             </Button>
             <Button style={{marginTop: '40px', fontSize: '1.1em'}} className='side-button' variant="contained" color="secondary">
-            Change Menu
+            Change Menu <RestaurantMenuIcon/>
             </Button>
             <Button style={{marginTop: '40px', fontSize: '1.1em'}} className='side-button' variant="contained" color="secondary">
-            Update Ingredients
+            Update Ingredients <SetMealIcon/>
             </Button>
             </div>
         </div>
