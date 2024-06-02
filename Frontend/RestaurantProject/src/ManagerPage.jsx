6f4b0ee2e07f01922e981096f4ab2bf6468bd8d6
TzurelRauper <tzurel150@gmail.com>
2024-06-02 19:18:28 +0300

    add paper to ManagerPage.jsx

--- Frontend/RestaurantProject/src/ManagerPage.jsx
+++ Frontend/RestaurantProject/src/ManagerPage.jsx
@@ -4,13 +4,14 @@ import TableRestaurantIcon from '@mui/icons-material/TableRestaurant';
 import FastfoodIcon from '@mui/icons-material/Fastfood';
 import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
 import SetMealIcon from '@mui/icons-material/SetMeal';
+import Paper from '@mui/material/Paper';
 
 const ManagerPage = () => {
     
-    
+
 
     return (
-        <div className='managerPageDiv'>
+        <div>
             <div className='buttonsManagerPage'>
             <Button style={{fontSize: '1.1em'}} className='side-button' variant="contained" color="secondary">
             Get Orders <FastfoodIcon/>
@@ -25,6 +26,7 @@ const ManagerPage = () => {
             Update Ingredients <SetMealIcon/>
             </Button>
             </div>
+            <Paper elevation={0}>Im Paper asdasdasdasgdfalskdjfhlkjabsd<br/> asdasd<br/> asdasd<br/> asdasd<br/> asdasd</Paper>
         </div>
     )
 }
