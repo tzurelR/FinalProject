9986af11d92fbb1bf822031e82a79c23bf580cae
TzurelRauper <tzurel150@gmail.com>
2024-06-02 18:59:52 +0300

    add buttons to ManagerPage.jsx

--- Frontend/RestaurantProject/src/ManagerPage.jsx
+++ Frontend/RestaurantProject/src/ManagerPage.jsx
@@ -1,7 +1,24 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 const ManagerPage = () => {
     return (
-        <div>create manger page branch</div>
+        <div className='managerPageDiv'>
+            <div className='buttonsManagerPage'>
+            <Button style={{fontSize: '1.1em'}} className='side-button' variant="contained" color="secondary">
+            Get Orders
+            </Button>
+            <Button style={{marginTop: '40px', fontSize: '1.1em'}} className='side-button' variant="contained" color="secondary">
+            Get Table Reservations
+            </Button>
+            <Button style={{marginTop: '40px', fontSize: '1.1em'}} className='side-button' variant="contained" color="secondary">
+            Change Menu
+            </Button>
+            <Button style={{marginTop: '40px', fontSize: '1.1em'}} className='side-button' variant="contained" color="secondary">
+            Update Ingredients
+            </Button>
+            </div>
+        </div>
     )
 }
 
