b4add8e16e056fe98d257c0ab07cd325ebbca45f
TzurelRauper <tzurel150@gmail.com>
2024-06-13 17:15:32 +0300

    success to connect to GPT api

--- Backend/chatGptRequest.js
+++ Backend/chatGptRequest.js
@@ -1,1 +1,21 @@
+import { config } from "dotenv";
+import OpenAI from "openai/index.mjs";
+config();
+
+
 const apiKey = process.env.GPTAPI_KEY;
+
+const openai = new OpenAI({
+    apiKey: apiKey
+});
+
+const requestToGpt = async() => {
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: "user", content: "which day today in israel?" }],
+        model: "gpt-4o",
+      });
+    
+      console.log(completion.choices[0]);
+}
+
+export {requestToGpt};
