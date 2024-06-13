60c8a264aff18c30a76792088254c7f5014e50ae
TzurelRauper <tzurel150@gmail.com>
2024-06-13 11:57:23 +0300

    add image to AddIngredients.jsx

--- Frontend/RestaurantProject/src/AddIngredients.jsx
+++ Frontend/RestaurantProject/src/AddIngredients.jsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { useState } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function AddIngredients() {
     return (
-        <div>
+        <div className='AddIngredientsDiv'>
+            <button style={{backgroundColor: '#3FFF29', position: 'absolute', top: '0', left: '0', color: 'black'}}><CloseIcon/></button>
             test
         </div>
     )
