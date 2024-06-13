7c99f2338fb39847131048088f6ae353ceecca80
TzurelRauper <tzurel150@gmail.com>
2024-06-13 11:30:14 +0300

    add AddIngredients.jsx component

---
+++ Frontend/RestaurantProject/src/AddIngredients.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { useState } from 'react';
+
+export default function AddIngredients() {
+    return (
+        <div>
+            test
+        </div>
+    )
+}
