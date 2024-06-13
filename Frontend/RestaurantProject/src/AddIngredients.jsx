4ebd00a4ef4b622d888b956548b789e33510acc0
TzurelRauper <tzurel150@gmail.com>
2024-06-13 12:09:29 +0300

    add changes in style to AddIngredients.jsx

--- Frontend/RestaurantProject/src/AddIngredients.jsx
+++ Frontend/RestaurantProject/src/AddIngredients.jsx
@@ -6,7 +6,12 @@ export default function AddIngredients() {
     return (
         <div className='AddIngredientsDiv'>
             <button style={{backgroundColor: '#3FFF29', position: 'absolute', top: '0', left: '0', color: 'black'}}><CloseIcon/></button>
-            test
+            <div style={{position: 'absolute', top: '50%', left: '35%'}}>
+                <input placeholder='ingredient name' style={{backgroundColor: 'white', color: 'black', height: '25px'}}/>
+            </div>
+            <div style={{position: 'absolute', top: '60%', left: '35%',}}>
+                <input placeholder='amount' style={{backgroundColor: 'white', color: 'black', height: '25px'}}/>
+            </div>
         </div>
     )
 }
