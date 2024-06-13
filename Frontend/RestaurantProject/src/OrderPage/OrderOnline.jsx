8ff72e5e6b23e6c3a4da0e47d96c864efd6a98d2
TzurelRauper <tzurel150@gmail.com>
2024-06-13 15:27:56 +0300

    add changes to Chat.jsx

--- Frontend/RestaurantProject/src/OrderPage/OrderOnline.jsx
+++ Frontend/RestaurantProject/src/OrderPage/OrderOnline.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
+import Chat from '../Chat';
 import Accordion from '@mui/material/Accordion';
 import AccordionActions from '@mui/material/AccordionActions';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -111,126 +112,8 @@ export default function OrderOnline() {
       </Accordion>
       ))}
 
-      {/* <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1-content"
-          id="panel1-header"
-        >
-          Israeli salad {israeliSaladCost}$
-        </AccordionSummary>
-        <AccordionDetails>
-          refreshing and colorful dish made from diced tomatoes, and cucumbers, dressed with olive oil, lemon juice, salt, and pepper.
-        </AccordionDetails>
-        <img src = {israeliSaladImg} style={{ width: '250px', height: '150px' }}/>
-        <div style={{ display: 'flex' }}>
-        <Button variant="contained" onClick={() => setCount('israeliSalad', '+')}>+</Button>
-        <h4 style={{ marginLeft: '20px', marginRight: '20px'}}>{israeliSaladCount}</h4>
-        <Button variant="contained" onClick={() => setCount('israeliSalad', '-')}>-</Button>
-        </div>
-      </Accordion>
-      
-    
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2-content"
-          id="panel2-header"
-        >
-          ARAIS {araisCost}$
-        </AccordionSummary>
-        <AccordionDetails>
-          made with seasoned ground meat (typically lamb or beef) stuffed inside pita bread and toasted until crispy
-        </AccordionDetails>
-        <img src = {araisImg} style={{ width: '300px', height: '200px' }}/>
-        <div style={{ display: 'flex' }}>
-        <Button variant="contained" onClick={() => setCount('arais', '+')}>+</Button>
-        <h4 style={{ marginLeft: '20px', marginRight: '20px'}}>{araisCount}</h4>
-        <Button variant="contained" onClick={() => setCount('arais', '-')}>-</Button>
-        </div>
-      </Accordion>
-
-      <div>
-        <h4>MAINS</h4>
-      </div>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2-content"
-          id="panel2-header"
-        >
-          Grilled pullet {grilledPulletCost}$
-        </AccordionSummary>
-        <AccordionDetails>
-          young chicken cooked over an open flame or on a grill
-        </AccordionDetails>
-        <img src = {grilledPulletImg} style={{ width: '250px', height: '150px' }}/>
-        <div style={{ display: 'flex' }}>
-        <Button variant="contained" onClick={() => setCount('grilledPullet', '+')}>+</Button>
-        <h4 style={{ marginLeft: '20px', marginRight: '20px'}}>{grilledPulletCount}</h4>
-        <Button variant="contained" onClick={() => setCount('grilledPullet', '-')}>-</Button>
-        </div>
-      </Accordion>
-
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2-content"
-          id="panel2-header"
-        >
-          Hamburger {hamburgerCost}$
-        </AccordionSummary>
-        <AccordionDetails>
-          Our classic hamburger features a succulent beef patty, ripe tomatoes, and tangy pickles, all sandwiched between soft brioche buns, served with a side of golden fries for a timeless taste of comfort.
-        </AccordionDetails>
-        <img src = {hamburgerImg} style={{ width: '250px', height: '150px' }}/>
-        <div style={{ display: 'flex' }}>
-        <Button variant="contained" onClick={() => setCount('hamburger', '+')}>+</Button>
-        <h4 style={{ marginLeft: '20px', marginRight: '20px'}}>{hamburgerCount}</h4>
-        <Button variant="contained" onClick={() => setCount('hamburger', '-')}>-</Button>
-        </div>
-      </Accordion>
-
-      <div>
-        <h4>DRNIKS</h4>
-      </div>
-
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2-content"
-          id="panel2-header"
-        >
-          Cola 3$
-        </AccordionSummary>
-        <img src = {colaImg} style={{ width: '200px', height: '200px' }}/>
-        <div style={{ display: 'flex' }}>
-        <Button variant="contained" onClick={() => setCount('cola', '+')}>+</Button>
-        <h4 style={{ marginLeft: '20px', marginRight: '20px'}}>{colaCount}</h4>
-        <Button variant="contained" onClick={() => setCount('cola', '-')}>-</Button>
-        </div>
-      </Accordion>
-
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2-content"
-          id="panel2-header"
-        >
-          Beer {beerCost}$
-        </AccordionSummary>
-        <AccordionDetails>
-          Crafted in-house, our refreshing brew offers a delightful balance of flavors, perfect for sipping and savoring in the comfort of your own space.
-        </AccordionDetails>
-        <img src = {beerImg} style={{ width: '250px', height: '200px' }}/>
-        <div style={{ display: 'flex' }}>
-        <Button variant="contained" onClick={() => setCount('beer', '+')}>+</Button>
-        <h4 style={{ marginLeft: '20px', marginRight: '20px'}}>{beerCount}</h4>
-        <Button variant="contained" onClick={() => setCount('beer', '-')}>-</Button>
-        </div>
-      </Accordion> */}
-
       <Button variant="contained" style={{marginTop:'40px'}} onClick={goToPay}>PAY</Button>
+      <Chat />
     </div>
   );
 }
