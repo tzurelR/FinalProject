154a9cab72c21f419b4b6ac81f429344b6f4e9b3
TzurelRauper <tzurel150@gmail.com>
2024-06-13 12:16:37 +0300

    finish style the AddIngredients.jsx

--- Frontend/RestaurantProject/src/AddIngredients.jsx
+++ Frontend/RestaurantProject/src/AddIngredients.jsx
@@ -3,15 +3,21 @@ import { useState } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
 
 export default function AddIngredients() {
+
+    const [ingredientName, setIngredientName] = useState('');
+    const [amount, setAmount] = useState(0);
+
+
     return (
         <div className='AddIngredientsDiv'>
-            <button style={{backgroundColor: '#3FFF29', position: 'absolute', top: '0', left: '0', color: 'black'}}><CloseIcon/></button>
+            <button style={{backgroundColor: '#FF9E9E', position: 'absolute', top: '0', left: '0', color: 'black'}}><CloseIcon/></button>
             <div style={{position: 'absolute', top: '50%', left: '35%'}}>
                 <input placeholder='ingredient name' style={{backgroundColor: 'white', color: 'black', height: '25px'}}/>
             </div>
             <div style={{position: 'absolute', top: '60%', left: '35%',}}>
                 <input placeholder='amount' style={{backgroundColor: 'white', color: 'black', height: '25px'}}/>
             </div>
+            <button style={{ position: 'absolute', bottom: '10px', right: '10px', backgroundColor: '#28a745'}}>Add</button>
         </div>
     )
 }
