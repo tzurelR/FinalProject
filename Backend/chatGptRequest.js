2523cd961ee7e77f2e4bd0656cf9a19a18ec9d6f
TzurelRauper <tzurel150@gmail.com>
2024-06-14 16:57:39 +0300

    get fitsr message from chatGpt that asking 3 question that the user need to answer.

--- Backend/chatGptRequest.js
+++ Backend/chatGptRequest.js
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 import OpenAI from "openai/index.mjs";
 config();
 
+const firstMsgToGPT = 'Hello, I have a restaurant. I will say you what my dishes and then you will ask my user 3 short questions and suggest 2 dishes. this are dishes: Israeli, Salad Arais, Grilled Pullet, Hamburger, Cola, Beer. Just give the questions with no openning or suggest yet';
 
 const apiKey = process.env.GPTAPI_KEY;
 
@@ -9,13 +10,25 @@ const openai = new OpenAI({
     apiKey: apiKey
 });
 
-const requestToGpt = async() => {
+const requestToGpt = async(req, res) => {
+    
     const completion = await openai.chat.completions.create({
-        messages: [{ role: "user", content: "which day today in israel?" }],
+        messages: [{ role: "user", content: firstMsgToGPT }],
         model: "gpt-4o",
       });
-    
-      console.log(completion.choices[0]);
+      console.log('GPT:', completion.choices[0].message.content);
 }
 
-export {requestToGpt};
+const getFirstMsgFromGpt = async(req, res) => {
+    try {
+        const completion = await openai.chat.completions.create({
+            messages: [{ role: "user", content: firstMsgToGPT }],
+            model: "gpt-4o",
+          });
+          res.json({message: completion.choices[0].message.content});
+    } catch(error) {
+        res.json({message: error.message});
+    }
+}
+
+export {requestToGpt, getFirstMsgFromGpt};
--- Frontend/RestaurantProject/src/ChatBox/ChatBubble.jsx
+++ Frontend/RestaurantProject/src/ChatBox/ChatBubble.jsx
@@ -3,14 +3,13 @@ import LaptopIcon from '@mui/icons-material/Laptop';
 import PersonIcon from '@mui/icons-material/Person';
 
 export default function ChatBubble (props) {
-    console.log(props.position);
     const bubbleStyle = {
-        backgroundColor: props.position === 'right' ? '#DCF8C6' : '#F0F0F0',
+        backgroundColor: props.position === 'right' ? '#F0F0F0' : '#DCF8C6',
         borderRadius: '8px',
         padding: '8px',
         marginBottom: '8px',
         maxWidth: '50%',
-        alignSelf: props.position === 'right' ? 'flex-start' : 'flex-end',
+        alignSelf: props.position === 'right' ? 'flex-end' : 'flex-start',
         whiteSpace: 'pre-wrap',
         wordBreak: 'break-word',
       };
